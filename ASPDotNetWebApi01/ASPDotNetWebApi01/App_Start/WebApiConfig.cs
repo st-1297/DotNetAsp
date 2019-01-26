098aad778f18b2ed50f1bdbb66b321ae7ac687fc
st-1297 <sooooota@hotmail.com>
2019-01-26 18:04:34 +0900

    初期化

---
+++ ASPDotNetWebApi01/ASPDotNetWebApi01/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace ASPDotNetWebApi01
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API の設定およびサービス
+
+            // Web API ルート
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // XMLではなく、JSONを返すよう設定
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
